145a3de9df44dd5b5b4a97e6328bdc43c1d17804
Oscar <oscar.w.roche@gmail.com>
2020-09-24 16:58:13 +0200

    refactoring

---
+++ dist/constants/constants.d.ts
@@ -0,0 +1,19 @@
+export declare const constants: {
+    GelatoCore: string;
+    ProviderModuleDSA: string;
+    DAI: string;
+    DAI_UNISWAP: string;
+    CDAI: string;
+    InstaIndex: string;
+    InstaList: string;
+    InstaConnectors: string;
+    InstaAccount: string;
+    ConnectAuth: string;
+    ConnectBasic: string;
+    ConnectMaker: string;
+    ConnectCompound: string;
+    ConnectGelato: string;
+    ConnectInstaPool: string;
+    InstaCompoundResolver: string;
+    InstaMakerResolver: string;
+};
---
+++ dist/lib/utils/abiEncodeWithSelector.d.ts
@@ -0,0 +1,1 @@
+export declare function abiEncodeWithSelector(abi: any, functionName: string, inputs?: any[]): Promise<any>;
