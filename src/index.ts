ac30299e6f710c281cc0201cd936cdac020e24cf
joelamouche <estienne.antoine@gmail.com>
2020-10-12 18:31:20 +0200

    add exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { createDSA } from "./lib/createDSA";
 export { createGelatoOptimizer } from "./lib/createGelatoOptimizer";
 export { getMainnetAddresses } from "./lib/getMainnetAddresses";
 export { getDSAAccounts } from "./lib/getDSAAccounts";
+export { getVaults } from "./lib/getVaults";
+export { sendDSA } from "./lib/sendDSA";
