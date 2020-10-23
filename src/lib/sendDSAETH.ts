eb14905ffee6f7b57d08f31591fe741a2fb86f84
joelamouche <estienne.antoine@gmail.com>
2020-10-23 15:35:18 +0200

    withdraw and send eth

--- src/lib/sendDSAETH.ts
+++ src/lib/sendDSAETH.ts
@@ -1,12 +1,12 @@
 import { ethers, BigNumber, utils } from "ethers";
 import Web3 from "web3";
 
-export async function sendDSA(web3: Web3, dsaAddress:string, eth_amount:number) {
+export async function sendDSAETH(web3: Web3, dsaAddress:string, eth_amount:number) {
   //setup ethers
   let provider = new ethers.providers.Web3Provider(web3.currentProvider as any);
   let userWallet: ethers.providers.JsonRpcSigner = await provider.getSigner();
 
-  // And send ETH to the DSA, just in case
+  // And send ETH to the DSA
   const gasLimit = BigNumber.from(1000000);
   const gasPrice = utils.parseUnits("20", "gwei");
   return await userWallet.sendTransaction({
